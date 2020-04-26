222d9af2bdd524d393d77c09fb6e3340b64d43e6
Varun Batra <59274429+Varun-Batra@users.noreply.github.com>
2020-04-26 11:52:46 +0530

    Add files via upload

---
+++ Writing and Understanding the C Program/Writing and Understanding the C Program.c
@@ -0,0 +1,10 @@
+/*Program Name: Writing and Understanding the C-Program.
+Author: Varun Batra || MechE Inside
+Output of Program: String on Console Output.*/
+
+#include<stdio.h>
+
+main()
+{
+	printf("Welcome to my channel MechE Inside.");
+}
